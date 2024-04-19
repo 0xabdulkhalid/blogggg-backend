4522494e8f912001cb061bfd9a1e04d2055a063c
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-04-19 16:43:48 +0530

    feat: implement post listing and viewing

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,7 +2,6 @@ const asyncHandler = require("express-async-handler");
 const Post = require("../models/post");
 
 exports.create_post = asyncHandler(async (req, res, next) => {
-  console.log(req.user);
   // Check if the user is an admin
   if (!req.user && !req.user.isAdmin) {
     return res
@@ -17,6 +16,17 @@ exports.create_post = asyncHandler(async (req, res, next) => {
   });
 
   const result = await post.save();
-
   return res.status(200).send("success");
+});
+
+exports.list_posts = asyncHandler(async (req, res, next) => {
+  const posts = await Post.find({ isPublished: true }, { content: 0 })
+    .select("title createdAt")
+    .exec();
+  return res.status(200).json(posts);
+});
+
+exports.view_post = asyncHandler(async (req, res, next) => {
+  const post = await Post.findById(req.params.id);
+  return res.status(200).json(post);
 });
--- routes/post.js
+++ routes/post.js
@@ -4,4 +4,8 @@ const postController = require("../controllers/postController");
 
 router.post("/create-post", postController.create_post);
 
+router.get("/list-posts", postController.list_posts);
+
+router.get("/view-post/:id", postController.view_post);
+
 module.exports = router;
